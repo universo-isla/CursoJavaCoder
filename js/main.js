929cc1ce0c187cfe356e5d31822f8b433cdef868
Aestrada <ordning.estrada@gmail.com>
2022-02-16 09:19:50 -0600

    error quitar carrito

--- js/main.js
+++ js/main.js
@@ -3,25 +3,25 @@ let suma= 0;
 let costoEnvioForaneo = 0;
 let costoEnvioLocal = 0; */
 
+let totalCompra = 0;
 
 const contenedorPlantas = document.querySelector('.contenedorPlantas');
+const carrito = document.querySelector('.carrito');
+const contadorCarrito = document.getElementById('.contadorCarrito');
 
 const plantas = [
-    {id: 1, nombre: "Helecho azul", precio: 150, img: '../multimd/helecho_azul.png'},
-    {id: 2, nombre: "Helecho Osaka", precio: 170, img: '../multimd/helecho_osaka_ok.png'},
-    {id: 3, nombre: "Helecho Faurei", precio: 150, img: '../multimd/helecho_fua.png'},
-    {id: 4, nombre: "Uña de señorita", precio: 120, img: '../multimd/una_srita.png'},
-    {id: 5, nombre: "Begonia Cruz de Hierro", precio: 120, img: '../multimd/begonia_cdh.png'},
-    {id: 6, nombre: "Peperomia", precio: 120, img: '../multimd/peperomia.png'},
+    {id: 1, nombre: "Helecho Azul", precio: 150, img: '../multimd/helecho_azul.png', cantidad:1},
+    {id: 2, nombre: "Helecho Osaka", precio: 170, img: '../multimd/helecho_osaka_ok.png', cantidad:1},
+    {id: 3, nombre: "Helecho Faurei", precio: 150, img: '../multimd/helecho_fua.png', cantidad:1},
+    {id: 4, nombre: "Uña de señorita", precio: 120, img: '../multimd/una_srita.png', cantidad:1},
+    {id: 5, nombre: "Begonia Cruz de Hierro", precio: 120, img: '../multimd/begonia_cdh.png', cantidad:1},
+    {id: 6, nombre: "Peperomia", precio: 120, img: '../multimd/peperomia.png', cantidad:1},
     // {id: 7, nombre: "Sanseveira", precio: 120}
 ]
-compras = [];
+
+arrayCarrito = [];
 mostrarPlantas();
 total =0;
-
-
-
-
 
 
 function mostrarPlantas() {
@@ -48,26 +48,91 @@ function mostrarPlantas() {
         btnComprar.classList.add("btn-comprar");
         btnComprar.textContent = "Agregar al carrito";
 
-        btnComprar.onclick = () => {
-            agregarACarrito(planta.id);
-        };
-
         divPlanta.appendChild(imagenPlanta);
         divPlanta.appendChild(nombrePlanta);
         divPlanta.appendChild(precioPlanta);
         divPlanta.appendChild(btnComprar);
         contenedorPlantas.appendChild(divPlanta);
 
+        btnComprar.onclick = () => {
+            agregarACarrito(planta.id);
+        };
+
+
     })
 }
 
 
-console.log("Bienvenida a nuestro carrito de compras, da click en el producto que quieres agregar");
+/* function agregarACarrito(id){
+    const plantaAgregada = plantas.find( planta => planta.id == id )
+    alert("Agregaste " + plantaAgregada.nombre + " a tu carrito");
+    arrayCarrito.push(plantaAgregada);
+    console.log(arrayCarrito);
 
+    const divCarrito = document.createElement("div");
+        divCarrito.classList.add('.cardCarrito');
+
+    const nombreEnCarrito = document.createElement("h3");
+        nombreEnCarrito.textContent = plantaAgregda.nombre;
+    
+} */
 
 function agregarACarrito(id){
-    const plantaAgregada = plantas.find( planta => planta.id === id );
+    let plantaAgregada = plantas.find( planta => planta.id == id )
+    console.log(plantaAgregada);
+    arrayCarrito.push(plantaAgregada);
 
-    console.log("Agregaste " + plantaAgregada.nombre + " a tu carrito, su costo es de $" + plantaAgregada.precio);
+   // alert("Agregaste " + plantaAgregada.nombre + " a tu carrito");
+   actualizarCarrito()
     
+    let tr = document.createElement("tr")
+    tr.className = "celdaCarrito"
+    tr.innerHTML = `
+            <td class="plantaTabla">${plantaAgregada.nombre}</td>
+            <td>$ ${plantaAgregada.precio}</td>
+            <td>cant.: ${plantaAgregada.cantidad}</td>
+            <td> <button id="btnQuitar${plantaAgregada.id}"> quitar </button> </td>          
+            `
+    carrito.appendChild(tr)
+
+    let btnQuitar = document.getElementById(`btnQuitar${plantaAgregada.id}`)
+    
+
+    btnQuitar.addEventListener('click', () => {
+        //btnQuitar.parentElement.remove()
+        console.log(btnQuitar.parentElement); //parentElement visualiza el elemento padre... si es que funciona
+    }) 
 }
+
+function actualizarCarrito (){
+    contadorCarrito.innerText = arrayCarrito.reduce((acc,el) => acc + el.cantidad, 0) //acumulador (acc) y elemento (el): actualiza, al recibir un elemento, el valor cantidad, 0 es para decir 0+1 y de ahí sigue sumando
+    totalCompra.innerText = arrayCarrito.reduce((acc, el) => acc + (el.precio * el.cantidad), 0)
+}
+
+
+   //console.log(arrayCarrito);   
+/*     function construirTabla(comprando) {
+        var tbl = document.getElementById("carrito");
+            var row = tbl.insertRow();
+    } */
+    
+    //alert(tbl);
+    
+    /* var cell1 = row.insertCell();
+    var cell2 = row.insertCell();
+    var cell3 = row.insertCell();
+    var cell4 = row.insertCell(); */
+
+/*     cell1.innerHTML = `<p>${plantaAgregar.nombre}</p> `
+    cell2.innerHTML = `<p>${plantaAgregar.precio}</p> `
+    cell1.innerHTML = `<button class="quitarDelCarrito"></button> ` */
+
+    
+/*     let div = document.createElement('div')
+    div.className = 'plantaEnCarrito'
+    div.innerHTML = `
+        <p>${plantaAgregar.nombre}</p>
+        <p>${plantaAgregar.precio}</p>
+        <button class="quitarDelCarrito"></button> `
+
+    carrito.appendChild(div); */
